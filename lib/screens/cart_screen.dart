9e9a7663eac6d6a790f21aa5f11c9d484de561f6
Gabriel <gabriel.soares14563@gmail.com>
2022-06-09 13:13:34 -0300

    b

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,9 +4,14 @@ import 'package:loja/models/user_model.dart';
 import 'package:loja/screens/login_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,71 +25,73 @@ class CartScreen extends StatelessWidget {
             alignment: Alignment.center,
             child: ScopedModelDescendant<CartModel>(
                 builder: (context, child, model) {
-              int p = model.products.length;
-              return Text(
-                "${p ?? 0} ${p == 1 ? "ITEM" : "ITENS"}",
-                style: const TextStyle(fontSize: 17),
-              );
-            }),
+                  int p = model.products.length;
+                  return Text(
+                    "${p ?? 0} ${p == 1 ? "ITEM" : "ITENS"}",
+                    style: const TextStyle(fontSize: 17),
+                  );
+                }),
           )
         ],
       ),
-      body: ScopedModelDescendant<CartModel>(builder: (context, child, model) {
-        if (model.isLoading && UserModel.of(context).isLoggedIn()) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (!UserModel.of(context).isLoggedIn()) {
-          return Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.remove_shopping_cart,
-                  size: 80,
-                  color: Theme.of(context).primaryColor,
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  "Faça o login para adicionar produtos!",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const LoginScreen()));
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Theme.of(context).primaryColor),
+      body: ScopedModelDescendant<CartModel>(
+        builder: (context, child, model) {
+          if (model.isLoading && UserModel.of(context).isLoggedIn()) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (!UserModel.of(context).isLoggedIn()) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.remove_shopping_cart,
+                    size: 80,
+                    color: Theme.of(context).primaryColor,
                   ),
-                  child: const Text(
-                    "Entrar",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Faça o login para adicionar produtos!",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
                   ),
-                )
-              ],
-            ),
-          );
-        } else if (model.products.isEmpty) {
-          return const Center(
-            child: Text(
-              "Nenhum produto no carrinho!",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const LoginScreen()));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Theme.of(context).primaryColor),
+                    ),
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  )
+                ],
               ),
-              textAlign: TextAlign.center,
-            ),
-          );
-        } else {
-          return Container();
-        }
-      }),
+            );
+          } else if (model.products.isEmpty) {
+            return const Center(
+              child: Text(
+                "Nenhum produto no carrinho!",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
